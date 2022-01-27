6fc84ece1ddcab4db6730c93a545fed02bd54d3f
Justine <98328323+JustineD22@users.noreply.github.com>
2022-01-27 15:07:58 +0100

    ajout et remplissage du head.php, main.php et footer.php

---
+++ includes/main.php
@@ -0,0 +1,2 @@
+<h1> GameLib </h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio vero dicta nemo. Ad, rem adipisci quod placeat blanditiis, neque laudantium maiores nostrum repellendus sed, iure culpa unde voluptas eligendi ipsum.</p>
