f62f31102849464606e383fc4429f6b8374b1f82
Justine <98328323+JustineD22@users.noreply.github.com>
2022-01-27 15:39:14 +0100

    inclusion nav.php dans main.php

--- includes/main.php
+++ includes/main.php
@@ -1,2 +1,6 @@
+<?php
+require_once './includes/nav.php';
+?>
+
 <h1> GameLib </h1>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio vero dicta nemo. Ad, rem adipisci quod placeat blanditiis, neque laudantium maiores nostrum repellendus sed, iure culpa unde voluptas eligendi ipsum.</p>
