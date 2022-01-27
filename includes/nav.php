9d699a8d5ee167bfca1e33b320b3c9ffcd11c0d1
Justine <98328323+JustineD22@users.noreply.github.com>
2022-01-27 15:37:16 +0100

    ajout fichier nav.php

---
+++ includes/nav.php
@@ -0,0 +1,7 @@
+<header>
+    <nav>
+        <ul>
+            <li> <a href="index.php?page=home"> Home </a> </li>
+            <li> <a href="index.php?page=news"> News </a> </li>
+            <li> <a href="index.php?page=contact"> Contact </a> </li>
+        </ul>
