1c50d2e5c6c50534bdfb54d1ac4f8b20f5545c6c
Justine <98328323+JustineD22@users.noreply.github.com>
2022-01-27 17:01:08 +0100

    creation autoload et inclusionInc

---
+++ functions/autoLoadFunction.php
@@ -0,0 +1,7 @@
+<?php
+$functionFiles = glob('./functions/*.php');
+for ($i = 0; $i < count($functionFiles); $i++) {
+    if ($functionFiles[$i] !== './functions/autoLoadFunction.php'){
+        require_once $functionFiles
+    }
+}
