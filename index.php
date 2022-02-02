4c312d356bfae2d12532f34dbd68db3f1150f36f
Justine <98328323+JustineD22@users.noreply.github.com>
2022-02-02 09:54:20 +0100

    ajout index

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 date_default_timezone_set('Europe/Paris');
 
+spl_autoload_register(function ($classname){
+    include './classes/' . $classname . '.php';
+});
+
 require_once './functions/autoLoadFunction.php';
 require_once './includes/head.php';
 require_once './includes/main.php';
