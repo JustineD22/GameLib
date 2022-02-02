413a43d7eb73ee0665e90e28242368cc8aa4ec70
Justine <98328323+JustineD22@users.noreply.github.com>
2022-02-02 11:37:54 +0100

    form

--- includes/formulaire.inc.php
+++ includes/formulaire.inc.php
@@ -1,45 +1,63 @@
 <?php
 if (isset($_POST['frm'])) {
-    $nom = trim($_POST['nom']) ?? '';
-    $prenom = trim($_POST['prenom']) ?? '';
-    $email = trim($_POST['email']) ?? '';
-    $mdp = trim($_POST['mdp']) ?? '';
-    $mdp2 = trim($_POST['mdp2']) ?? '';
+    $nom = htmlentities(trim($_POST['nom'])) ?? '';
+    $prenom = htmlentities(trim($_POST['prenom'])) ?? '';
+    $email = htmlentities(trim($_POST['email'])) ?? '';
+    $password = htmlentities(trim($_POST['mdp'])) ?? '';
+    $passwordverif = htmlentities(trim($_POST['mdp2'])) ?? '';
 
     $erreur = array();
 
-    if (strlen($nom)===0) {
-        array_push($erreur, "Veuiller saisir votre nom");
-    } 
-    elseif (!ctype_alpha($nom)) {
-        array_push($erreur, "Veuillez saisir une chaine de caractère alphabéthique");
-    }
-        
-    if (strlen($prenom)===0) {
-        array_push($erreur, "Veuiller saisir votre prenom");
-    }
-    elseif (!ctype_alpha($nom)) {
-        array_push($erreur, "Veuillez saisir une chaine de caractère alphabéthique");
-    }
-        
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      array_push($erreur, "Veuiller saisir un e-mail valide");  
-    }
+    if (strlen($nom) === 0)
+        array_push($erreur, "Veuillez saisir votre nom");
 
-    if (strlen($mdp)===0) {
+    elseif (!ctype_alpha($nom))
+        array_push($erreur, "Veuillez saisir des caractères alphabétiques");
+
+    if (strlen($prenom) === 0)
+        array_push($erreur, "Veuillez saisir votre prénom");
+
+    elseif (!ctype_alpha($prenom))
+        array_push($erreur, "Veuillez saisir des caractères alphabétiques");
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+        array_push($erreur, "Veuillez saisir un e-mail valide");
+
+    if (strlen($password) === 0)
         array_push($erreur, "Veuillez saisir un mot de passe");
-    }
 
-    if (strlen($mdp2)===0) {
-        array_push($erreur, "Veuillez confirmer votre mot de passe");
-    }
-    
-    
+    if (strlen($passwordverif) === 0)
+        array_push($erreur, "Veuillez saisir la vérification de votre mot de passe");
 
-    if (count($erreur)===0) {
-        echo "traitement du formulaire";
-    }
-    else {
+    if ($password !== $passwordverif)
+        array_push($erreur, "Vos mots de passe ne correspondent pas");
+
+    if (count($erreur) === 0) {
+        $serverName = "localhost";
+        $userName = "root";
+        $database = "exercice";
+        $userPassword = "";
+
+        try{
+            $conn = new PDO("mysql:host=$serverName;dbname=$database", $userName, $userPassword);
+            echo "Connexion OK";
+        }
+        catch(PDOException $e){
+            die("Erreur :  " . $e->getMessage());
+        }
+
+        $sql = "INSERT INTO utilisateurs(id_utilisateur, nom, prenom, mail, mdp)
+        VALUES (NULL, 'DURAND', 'Michel', 'michel@durand.com', '1234')";
+
+        $conn->exec($sql);
+
+
+        $conn = null;
+
+
+
+
+    } else {
         $messageErreur = "<ul>";
         $i = 0;
         do {
@@ -47,19 +65,22 @@ if (isset($_POST['frm'])) {
             $messageErreur .= $erreur[$i];
             $messageErreur .= "</li>";
             $i++;
-        }
-        while ($i < count($erreur));
+        } while ($i < count($erreur));
 
         $messageErreur .= "</ul>";
+
         echo $messageErreur;
+
+        echo password_hash($mdp,  PASSWORD_DEFAULT);
+
+
+
     }
-    
-    
-}
-else {
+} else {
     echo "Merci de renseigner le formulaire";
     $nom = $prenom = $email = '';
 }
+
 
 include './includes/frmFormulaire.php';
 
